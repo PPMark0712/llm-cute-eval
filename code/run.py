4ae909b6fb7831de6e028791b9d7482d1ea87828
JACKADOHAHA <1059370198@qq.com>
2024-05-21 18:01:51 +0800

    temp

--- code/run.py
+++ code/run.py
@@ -186,4 +186,5 @@ def main():
     
 
 if __name__ == "__main__":
+    print("222")
     main()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from code.run import main
 
 if __name__ == "__main__":
+    print("hello world!")
     main()
